1122e0fdb529880f1ff896dac8a709d3cacf808c
SquallStrifeX <squallstrifex@gmail.com>
2024-01-17 17:43:50 +0100

    qualcosa

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,11 +12,25 @@ export default {
 </script>
 
 <template lang="">
-    <div>
-        
+    <div class="container-lg">
+        <div class="row bg-danger p-1">
+            <div class="col-6"> 
+                <span class="news p-1 px-3">NEWS UPDATES</span>
+                <div>
+                <span>0:37</span>
+                <span>THE BEST HEALTHY FOOD</span>
+            </div></div>
+            <div class="col-2"></div>
+            <div class="col-4"></div>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-    
+    .news{
+        background-color: gray;
+        color: white;
+        font-weight: bolder;
+
+    }
 </style>
