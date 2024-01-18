699e2a10262a78a5bf424b58198daef361ce43bb
asafcastigliano <asafcastigliano@gmail.com>
2024-01-18 12:14:20 +0100

    finita colonna per la newsletter e inzio ultima sezione

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -13,34 +13,44 @@ export default {
 
 <template lang="">
     <div class="ultracontainer pb-4">
-        <div class="justify-content-center d-flex mb-5">
-            <div class="triangolo"></div>
+        <div class="justify-content-center d-flex">
+            <div class="triangle"></div>
         </div>
         
+        <div class="container p-5">
+            <div class="row">
+                <div class="col-4 d-flex">
+                    <div class="col-6" v-for="(section, index) in store.footerData.slice(0, 2)" :key="index">
+                        <h4>{{ section.title }}</h4>
+                        <ul>
+                            <li v-for="(link, linkIndex) in section.links" :key="linkIndex">
+                            <a :href="link.path">{{ link.text }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-4 d-flex">
+                    <div class="col-6" v-for="(section, index) in store.footerData.slice(2, 4)" :key="index">
+                        <h4>{{ section.title }}</h4>
+                        <ul>
+                            <li v-for="(link, linkIndex) in section.links" :key="linkIndex">
+                            <a :href="link.path">{{ link.text }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <h4>SUBCRIBE TO NEWSLETTER</h4>
+                    <input type="text" placeholder="Email" class="mb-2 input-subscribe w-100"><br>
+                    <button class="btn-subscribe w-100">SUBSCRIBE</button>
+                </div>
+            </div>
+        </div>
+
         <div class="container">
             <div class="row">
-                <div class="col-md-4 d-flex">
-                    <div class="col-md-6" v-for="(section, index) in store.footerData.slice(0, 2)" :key="index">
-                        <h4>{{ section.title }}</h4>
-                        <ul>
-                            <li v-for="(link, linkIndex) in section.links" :key="linkIndex">
-                            <a :href="link.path">{{ link.text }}</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-4 d-flex">
-                    <div class="col-md-6" v-for="(section, index) in store.footerData.slice(2, 4)" :key="index">
-                        <h4>{{ section.title }}</h4>
-                        <ul>
-                            <li v-for="(link, linkIndex) in section.links" :key="linkIndex">
-                            <a :href="link.path">{{ link.text }}</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-4 text-center">
-                    <p>Terza colonna</p>
+                <div class="col-12">
+                    <hr class="w-100">
                 </div>
             </div>
         </div>
@@ -59,7 +69,7 @@ export default {
         border-top: 10px solid $my_black;
     }
 
-    .triangolo {
+    .triangle {
         width: 0;
         height: 0;
         border-left: 15px solid transparent;
@@ -71,6 +81,11 @@ export default {
         list-style: none;
         padding-left: 0;
     }
+    
+    h4{
+        font-size: medium;
+        font-weight: bold;
+    }
 
     a{
         text-decoration: none;
@@ -80,5 +95,25 @@ export default {
     a:hover{
         color: $my_black;
         transition: color 0.3s ease;
+    }
+
+    .btn-subscribe{
+        height: 45px;
+        border-radius: 10px;
+        border: none;
+        padding: 5px;
+        background-color: $my_black;
+    }
+
+    .input-subscribe{
+        height: 45px;
+        border-radius: 10px;
+        border: none;
+        padding: 5px;
+        color: $my_black;
+    }
+
+    hr{
+        opacity: 1;
     }
 </style>
